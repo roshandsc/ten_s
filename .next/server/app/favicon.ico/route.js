772ea6f63af299b3111f0dc2d9adecd10c1a3cf0
tessacloud2025-erp <tessacloud2025@gmail.com>
2025-10-21 12:39:39 +0530

    Updated Tessa Learn landing page

---
+++ .next/server/app/favicon.ico/route.js
@@ -0,0 +1,6 @@
+var R=require("../../chunks/[turbopack]_runtime.js")("server/app/favicon.ico/route.js")
+R.c("server/chunks/node_modules_next_518e21a9._.js")
+R.c("server/chunks/[root-of-the-server]__c2e30f97._.js")
+R.m("[project]/tessa_learn/.next-internal/server/app/favicon.ico/route/actions.js [app-rsc] (server actions loader, ecmascript)")
+R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/tessa_learn/src/app/favicon--route-entry.js [app-route] (ecmascript)\" } [app-route] (ecmascript)")
+module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/tessa_learn/src/app/favicon--route-entry.js [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
